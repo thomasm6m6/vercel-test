9e7e5abdbd36cf6ba5ffffb357f1bac3bff2ea03
Thomas M <thomasm6m6@gmail.com>
2025-06-16 12:35:04 -0700

    this time for sure

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
